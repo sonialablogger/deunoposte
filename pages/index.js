6262c3197e5e36bacc9a6f7c8e493590e4f2f5fe
sonialablogger <88657700+sonialablogger@users.noreply.github.com>
2021-08-09 12:44:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <img src="/resultado-do-jogo-do-bicho.JPG" width="100%">
         </p>
 
         <div className={styles.grid}>
