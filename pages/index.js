e49b6d1847ba6571bae9110891cfc5c156ab49de
sonialablogger <88657700+sonialablogger@users.noreply.github.com>
2021-08-09 12:05:13 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Resultados do Jogo do Bicho</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bem-vindo aos resultados do Jogo do Bicho
         </h1>
 
         <p className={styles.description}>
